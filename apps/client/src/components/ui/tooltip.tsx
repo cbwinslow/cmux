a3f2bf542e202288e2daf2eabbd30690b95741b8
Lawrence Chen <54008264+lawrencecchen@users.noreply.github.com>
2025-10-20 14:02:23 -0700

    diff heatmap qol

--- apps/client/src/components/ui/tooltip.tsx
+++ apps/client/src/components/ui/tooltip.tsx
@@ -49,7 +49,6 @@ function TooltipContent({
         {...props}
       >
         {children}
-        <TooltipPrimitive.Arrow className="bg-primary fill-primary z-[var(--z-modal)] size-2.5 translate-y-[calc(-50%_-_2px)] rotate-45 rounded-[2px] pointer-events-none select-none" />
       </TooltipPrimitive.Content>
     </TooltipPrimitive.Portal>
   );
